5dd252c909b7eff846acf4ab1fd1e6a27c003fe2
Jack Wright <1998jwright@gmail.com>
2021-10-14 18:02:49 +0100

    You can not specify the YouTube video URL in the /play command

--- src/commands/modules/play.ts
+++ src/commands/modules/play.ts
@@ -5,34 +5,56 @@ import ytdl from 'ytdl-core-discord';
 import { CommandHandler } from '../CommandHandler.types';
 
 const play: CommandHandler = async interaction => {
-	if (!interaction.guildId || !(interaction.member instanceof GuildMember)) return;
+	try {
+		// The user who sent the command
+		const guildMember = interaction.member;
+		if (!interaction.guildId || !(guildMember instanceof GuildMember)) {
+			return;
+		}
 
-	// The user who sent the command
-	const guildMember = interaction.member;
-	const voiceChannel = guildMember.voice.channel;
+		const voiceChannel = guildMember.voice.channel;
+		if (!voiceChannel) {
+			await interaction.reply('You must be connected for a voice channel for me to know where to join!');
+			return;
+		}
 
-	if (!voiceChannel) {
-		await interaction.reply('You must be connected for a voice channel for me to know where to join!');
-		return;
+		const adapterCreator = interaction.guild?.voiceAdapterCreator;
+		if (!adapterCreator) {
+			interaction.reply('Unable to create or retrieve the voice adapter. I cannot play the video.');
+			return;
+		}
+
+		const connectionOptions = {
+			guildId: interaction.guildId,
+			channelId: voiceChannel.id,
+			adapterCreator: interaction.guild?.voiceAdapterCreator as DiscordGatewayAdapterCreator
+		};
+
+		const youtubeUrl = interaction.options.get('url', true).value;
+		if (typeof youtubeUrl !== 'string') {
+			interaction.reply('Invalid argument provided. This issue must be reported to the bot developer, as it is a configuration issue on our end.');
+			return;
+		}
+
+		if (!ytdl.validateURL(youtubeUrl)) {
+			interaction.reply('Invalid URL. I cannot play that video.');
+			return;
+		}
+		await interaction.reply('Downloading YouTube video...');
+
+		const audioBitstream = await ytdl(youtubeUrl, { filter: 'audioonly' });
+		const player = globals.audioPlayer;
+		const connection = joinVoiceChannel(connectionOptions);
+		await interaction.editReply('Downloaded! Now I am preparing to stream...');
+
+		const audioResource = createAudioResource(audioBitstream);
+		connection.subscribe(player);
+
+		await interaction.editReply('All ready to go. Playing video.');
+		player.play(audioResource);
+	} catch (error) {
+		console.error(error);
 	}
-
-	const adapterCreator = interaction.guild?.voiceAdapterCreator;
-	if (!adapterCreator) return;
-
-	const connectionOptions = {
-		guildId: interaction.guildId,
-		channelId: voiceChannel.id,
-		adapterCreator: interaction.guild?.voiceAdapterCreator as DiscordGatewayAdapterCreator
-	};
-
-	const player = globals.audioPlayer;
-	const connection = joinVoiceChannel(connectionOptions);
-	const audioBitstream = await ytdl('https://www.youtube.com/watch?v=dQw4w9WgXcQ'); // Never gonna give you up!
-	const audioResource = createAudioResource(audioBitstream);
-	connection.subscribe(player);
-
-	player.play(audioResource);
-	interaction.reply('I am playing some audio!');
 };
 
 export default play;
--- src/config/modules/commands.ts
+++ src/config/modules/commands.ts
@@ -2,7 +2,10 @@ import { SlashCommandBuilder } from '@discordjs/builders';
 
 const commands = [
 	new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!'),
-	new SlashCommandBuilder().setName('play').setDescription('Play an audio clip!')
+	new SlashCommandBuilder()
+		.setName('play')
+		.setDescription('Play an audio clip!')
+		.addStringOption(option => option.setName('url').setDescription('The YouTube video URL').setRequired(true))
 ];
 
 export default commands.map(builder => builder.toJSON());
