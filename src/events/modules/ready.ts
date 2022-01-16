0bf70a10b2d3dbdc837c735fd6380a4e3d43a126
Jack Wright <1998jwright@gmail.com>
2022-01-16 17:37:15 +0000

    Moved log of env vars

--- src/events/modules/ready.ts
+++ src/events/modules/ready.ts
@@ -1,11 +1,8 @@
-import { config } from 'bot-config';
 import { registerCommands } from 'bot-functions';
 import { Client } from 'discord.js';
 
 const ready = async (client: Client) => {
 	console.log(`Bot logged in as ${client.user?.username}!`);
-	console.log(`Environment: ${config.environment}`);
-	console.log(`Redis host: ${config.redisHost}`);
 
 	client.user?.setActivity({
 		name: 'music | type "/"!',
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import { Client } from 'discord.js';
 
 const client = new Client({ intents: config.intents });
 
+console.log(`Environment: ${config.environment}`);
+console.log(`Redis host: ${config.redisHost}`);
+
 client.on('ready', () => ready(client));
 client.on('interactionCreate', interactionCreate);
 client.login(config.discordToken);
