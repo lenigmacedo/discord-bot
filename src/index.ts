05875b7882ce37a04c1f3bc7f716bfaeff6e1d05
Jack Wright <1998jwright@gmail.com>
2022-01-16 17:32:06 +0000

    Added additional debug line for Redis host

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 import { config } from 'bot-config';
 import { interactionCreate, ready } from 'bot-events';
-import { registerCommands } from 'bot-functions';
 import { Client } from 'discord.js';
 
 const client = new Client({ intents: config.intents });
-
-registerCommands();
 
 client.on('ready', () => ready(client));
 client.on('interactionCreate', interactionCreate);
